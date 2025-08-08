75ac7ff8548144abab6a639b2cbc245e1a06506b
boobee <s67319010006@ptnicec.ac.th>
2025-08-08 10:10:48 +0700

    03

---
+++ 03/app.js
@@ -0,0 +1,22 @@
+let name = "soosee";
+const age = 22;
+
+function greet(name) {
+    return `สวัสดี ${name}`;
+}
+
+console.log(greet("soosee"));
+
+let score = 80;
+if (score >= 80) {
+    console.log("Grade A+");
+} else {
+    console.log("This not A+");
+}
+
+const goals = ["Dev", "Syber", "Pogramming"];
+console.log(goals[0]);
+
+for (i = 0; i < goals.length; i++) {
+    console.log(goals[i]);
+}
