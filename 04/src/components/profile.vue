c322d5acb17964660cf5ece8dcc2a7b9451a77db
boobee <s67319010006@ptnicec.ac.th>
2025-08-08 14:41:25 +0700

    04

---
+++ 04/src/components/profile.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>{{ name }}</h1>
+        <input v-model="name" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref("soosee")
+</script>
